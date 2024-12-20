d49a8ef9d15b75f76739f0a6b85e2577ba3ffe6b
oliakiss34 <oliakiss34@gmail.com>
2024-12-20 11:38:47 +0500

    2

--- 03_lesson/Address.py
+++ 03_lesson/Address.py
@@ -1,13 +1,8 @@
 class Address:
-    def __init__(self,index,town,street,home,kvartira):
-          self.i = index
-          self.t = town
-          self.s = street
-          self.h = home
-          self.kv = kvartira
+    def __init__(self, index, town, street, home, kvartira):
+          self.index = index
+          self.town = town
+          self.street = street
+          self.home = home
+          self.kvartira = kvartira
     
-    index = "620000"
-    town = "Ekaterinburg"
-    street = "Lenina"
-    home = "165"
-    kvartira = "18"
--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -1,21 +1,8 @@
 from Address import Address
 from Mailing import Mailing
 
-to_address = Address
-from_address = Address
-to_address = 620000, "г. Екатеринбург", "ул. Ленина", 165, 18
-from_address = 620800, "г. Москва", "ул. Свердлова", 22, 45
-
-sending = Mailing
-sending(to_address, from_address, 1200, 1234567890)
-
-print(
-    "Отправление",
-    sending.track,
-    "из",
-    from_address,
-    "в",
-    to_address,
-    ". Стоимость",
-    sending.cost,
-    "рублей.",)
+to_address = Address(620000, "г. Екатеринбург", "ул. Ленина", 165, 18) 
+from_address = Address(620800, "г. Москва", "ул. Свердлова", 22, 45)
+mailing = Mailing(to_address, from_address, 1200, 1234567890)
+print(f"Отправление {mailing.track} из {mailing.from_address.index}, {mailing.from_address.town},"      f" {mailing.from_address.street}, {mailing.from_address.home} - {mailing.from_address.kvartira} "
+      f"в {mailing.to_address.index}, {mailing.to_address.town}, {mailing.to_address.street}, "      f"{mailing.to_address.home} - {mailing.to_address.kvartira}. Стоимость {mailing.cost} рублей.")
