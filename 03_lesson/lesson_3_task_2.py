cfbf3d9d16158905cdd1a2ac22bb0cfc1fa1430c
oliakiss34 <oliakiss34@gmail.com>
2024-12-20 11:23:07 +0500

    просто

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -1,15 +1,10 @@
 from smartphone import Smartphone
-phone1 = Smartphone
-phone2 = Smartphone
-phone3 = Smartphone
-phone4 = Smartphone
-phone5 = Smartphone
-phone1 = ("Apple", "5s", "+7999455587")
-phone2 = ("Lenovo", "20", "+79125696753")
-phone3 = ("Honor", "Deluxe", "+7908769898")
-phone4 = ("Samsung", "Galaxy", "+79757543211")
-phone5 = ("Redmi", "Note", "+79505676569")
+phone1 = Smartphone ("Apple", "5s", "+7999455587")
+phone2 = Smartphone ("Lenovo", "20", "+79125696753")
+phone3 = Smartphone ("Honor", "Deluxe", "+7908769898")
+phone4 = Smartphone ("Samsung", "Galaxy", "+79757543211")
+phone5 = Smartphone ("Redmi", "Note", "+79505676569")
 
 catalog = [phone1, phone2, phone3, phone4, phone5]
 for phones in catalog:
-    print(phones)
+    print(phones.mm + "-" + phones.m + "." + phones.n)
--- 03_lesson/smartphone.py
+++ 03_lesson/smartphone.py
@@ -1,6 +1,6 @@
 #Список объектов
 class Smartphone:
      def __init__(self, model, marka, nomer):
-        self.marka= marka
-        self.model = model
-        self.nomer = nomer
+        self.mm= marka
+        self.m = model
+        self.n = nomer
